4e7f7b8242f90d05ae69447891938799f1511cfd
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-04-11 17:34:15 -0300

    mejorando el maquetado, y surgen problemas en la logica

--- js/apps.js
+++ js/apps.js
@@ -1,16 +1,15 @@
 let btnComenzar = document.getElementById(`btnComenzar`)
 let adivinador = document.querySelector(`form`)
 let divParaAlert = document.getElementById(`divParaAlert`)
-btnComenzar.addEventListener(`click`, comenzarJuego);
+
 
 
 function comenzarJuego(){
-    console.log(`Estoy apretando el boton comenzar`);
     let contenedorJuego = document.getElementById(`contenedorJuego`)
     console.log(contenedorJuego)
     if(btnComenzar.innerHTML === `Comenzar a jugar!`){
-        btnComenzar.innerHTML = `Empezar de nuevo`;
-        btnComenzar.className = `btn btn-danger`;
+        btnComenzar.innerHTML = `Estás jugando`;
+        btnComenzar.className = `btn btn-outline-primary`;
         let numAleatorio = Math.round(Math.random() * (10 - 1) + 1);
         console.log(`Te tocó el número: ${numAleatorio}`);
         
@@ -20,9 +19,9 @@ function comenzarJuego(){
             console.log(`Estoy en la funcion del formulario`);
             let input = document.querySelector('input').value;
             console.log(input);
+            let mensajeAlerta = document.createElement(`div`)
             if(input >= 1 && input <= 10){
                 if(input == numAleatorio){
-                    let mensajeAlerta = document.createElement(`div`)
                     mensajeAlerta.innerHTML = `Ganaste!`
                     divParaAlert.appendChild(mensajeAlerta)
                     adivinador.reset();
@@ -30,13 +29,11 @@ function comenzarJuego(){
                     btnComenzar.className = `btn btn-primary`;
                 } else{
                     if(input < numAleatorio){
-                        let mensajeAlerta = document.createElement(`div`)
                         mensajeAlerta.innerHTML = `PISTA: el número mágico es más alto que ${input}!`
                         mensajeAlerta.className= `mb-2`
                         divParaAlert.appendChild(mensajeAlerta)
                         adivinador.reset();
                     } else if(input > numAleatorio){
-                        let mensajeAlerta = document.createElement(`div`)
                         mensajeAlerta.innerHTML=`PISTA: el número mágico es más bajo que ${input}!`
                         mensajeAlerta.className= `mb-2`
                         divParaAlert.appendChild(mensajeAlerta)
@@ -44,7 +41,6 @@ function comenzarJuego(){
                     }
                 }
             } else{
-                let mensajeAlerta = document.createElement(`div`)
                 mensajeAlerta.innerHTML=`Recordá que tenes que elegir un numero entre el 1 y el 10.`
                 mensajeAlerta.className= `mb-2`
                 divParaAlert.appendChild(mensajeAlerta)
