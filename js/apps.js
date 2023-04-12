0886342cd67fc53bae5eb3bd947d7c1a2d65049f
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-04-12 08:33:24 -0300

    se modificó la logica del ejercicio para que queden dos funciones diferenciadas: comenzar a jugar y adivinar numero, antes estaban en una sola

--- js/apps.js
+++ js/apps.js
@@ -1,58 +1,50 @@
+let numMagico
 let btnComenzar = document.getElementById(`btnComenzar`)
-let adivinador = document.querySelector(`form`)
-let divParaAlert = document.getElementById(`divParaAlert`)
-
-
+ /* let adivinador = document.querySelector(`form`)
+let divParaAlert = document.getElementById(`divParaAlert`) */
 
 function comenzarJuego(){
-    let contenedorJuego = document.getElementById(`contenedorJuego`)
-    console.log(contenedorJuego)
-    if(btnComenzar.innerHTML === `Comenzar a jugar!`){
         btnComenzar.innerHTML = `Estás jugando`;
-        btnComenzar.className = `btn btn-outline-primary`;
-        let numAleatorio = Math.round(Math.random() * (10 - 1) + 1);
-        console.log(`Te tocó el número: ${numAleatorio}`);
-        
-        adivinador.addEventListener(`submit`, adivinandoNumero)
-        function adivinandoNumero(e){
-            e.preventDefault();
-            console.log(`Estoy en la funcion del formulario`);
-            let input = document.querySelector('input').value;
-            console.log(input);
-            let mensajeAlerta = document.createElement(`div`)
-            if(input >= 1 && input <= 10){
-                if(input == numAleatorio){
-                    mensajeAlerta.innerHTML = `Ganaste!`
-                    divParaAlert.appendChild(mensajeAlerta)
-                    adivinador.reset();
-                    btnComenzar.innerHTML = `Comenzar a jugar!`;
-                    btnComenzar.className = `btn btn-primary`;
-                } else{
-                    if(input < numAleatorio){
-                        mensajeAlerta.innerHTML = `PISTA: el número mágico es más alto que ${input}!`
-                        mensajeAlerta.className= `mb-2`
-                        divParaAlert.appendChild(mensajeAlerta)
-                        adivinador.reset();
-                    } else if(input > numAleatorio){
-                        mensajeAlerta.innerHTML=`PISTA: el número mágico es más bajo que ${input}!`
-                        mensajeAlerta.className= `mb-2`
-                        divParaAlert.appendChild(mensajeAlerta)
-                        adivinador.reset();
-                    }
-                }
-            } else{
-                mensajeAlerta.innerHTML=`Recordá que tenes que elegir un numero entre el 1 y el 10.`
-                mensajeAlerta.className= `mb-2`
-                divParaAlert.appendChild(mensajeAlerta)
-                adivinador.reset();
-                btnComenzar.innerHTML = `Comenzar a jugar!`;
-                btnComenzar.className = `btn btn-primary`;
-            }
-        }
+        btnComenzar.className = `btn btnRosaDisabled`;
+        btnComenzar.disabled = true;
 
-    } else{
-        adivinador.reset();
-        btnComenzar.innerHTML = `Comenzar a jugar!`;
-        btnComenzar.className = `btn btn-primary`;
+        let numAleatorio = Math.round(Math.random() * (100 - 1) + 1);
+        numMagico = numAleatorio
+        console.log(`Te tocó el número: ${numMagico}`);
+        
+        let formJuego = document.getElementById(`formJuego`)
+        formJuego.reset();
     }
-}
+
+
+   function adivinandoNumero(e){
+    e.preventDefault();
+    console.log(`Estoy en la funcion del formulario`);
+    let input = document.querySelector('input').value;
+    console.log(input);
+    let msjAlert = document.getElementById(`divParaAlert`);
+    
+    if(input >= 1 && input <= 100){
+        if(input == numMagico){
+            msjAlert.innerHTML = `Ganaste!`
+            msjAlert.className = `alert alertRosa`
+            console.log(msjAlert)
+            btnComenzar.innerHTML = `Jugar de nuevo!`;
+            btnComenzar.className = `btn btnRosa`
+            btnComenzar.disabled = false;
+        } else if(input < numMagico){
+                msjAlert.innerHTML = `Volvé a intentarlo!
+                PISTA: el número mágico es más alto que ${input}!`
+                msjAlert.className = `alert alertSeguiParticipando`
+                console.log(msjAlert)
+            } else if(input > numMagico){
+                msjAlert.innerHTML=`Volvé a intentarlo!
+                PISTA: el número mágico es más bajo que ${input}!`
+                msjAlert.className = `alert alertSeguiParticipando`
+                console.log(msjAlert)
+            }    
+    } else{
+        msjAlert.innerHTML=`Recordá que tenes que elegir un numero entre el 1 y el 10.`
+        msjAlert.className = `alert alertSeguiParticipando`
+    }
+    }
